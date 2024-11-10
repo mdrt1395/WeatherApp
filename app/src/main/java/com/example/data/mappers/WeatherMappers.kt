1fd056ac3f825b47509dfdac8843e7ddcde17ed6
mdrt1395 <marcodavidrt95@gmail.com>
2024-11-10 07:41:00 -0600

    finished mappers

--- app/src/main/java/com/example/data/mappers/WeatherMappers.kt
+++ app/src/main/java/com/example/data/mappers/WeatherMappers.kt
@@ -3,7 +3,9 @@ package com.example.data.mappers
 import android.os.Build
 import androidx.annotation.RequiresApi
 import com.example.data.remote.WeatherDataDto
+import com.example.data.remote.WeatherDto
 import com.example.domain.weather.WeatherData
+import com.example.domain.weather.WeatherInfo
 import com.example.domain.weather.WeatherType
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -36,5 +38,22 @@ fun WeatherDataDto.toWeatherDataMap(): Map<Int, List<WeatherData>> {
         it.index / 24
     }.mapValues {
         it.value.map { it.data }
-    }.
+    }
+}
+
+fun WeatherDto.toWeatherInfo(): WeatherInfo {
+    val weatherDataMap = weatherData.toWeatherDataMap()
+    val now = LocalDateTime.now()
+    val currentWeatherData = weatherDataMap[0]?.find {
+        val hour = when {
+        now.minute < 30 -> now.hour
+        now.hour == 23 -> 0
+        else -> now.hour + 1
+    }
+        it.time.hour == hour
+    }
+    return WeatherInfo(
+        weatherDataPerDay = weatherDataMap,
+        currentWeatherData = currentWeatherData
+    )
 }
